ad91cc9b4d87b9dfc7d6f0d182f669dd4d6a4e00
Raymond Piller <VertigoRay@users.noreply.github.com>
2015-08-13 16:30:32 -0500

    Handle paths better.
    - Need the root directory to exist for *unzip* and `Resolve-Path`
    - Assumes `$requirements.Path` is always at least one folder and one file.

--- requirements.ps1
+++ requirements.ps1
@@ -1,9 +1,16 @@
 foreach ($requirement in (ConvertFrom-Json (Get-Content .\REQUIREMENTS.json | Out-String))) {
+    Write-Debug ($requirement | Out-String)
     if (-not (Get-Command $requirement.Command -ErrorAction Ignore)) {
+        Write-Debug ($requirement | Out-String)
         if (-not (Test-Path ($requirement.Path -f (Invoke-Expression $requirement.Path_f)) -ErrorAction Ignore)) {
             Invoke-WebRequest ($requirement.URL -f (Invoke-Expression $requirement.URL_f)) -OutFile 'requirement.zip' -UseBasicParsing
+
+            $Parent = (Split-Path (Split-Path ($requirement.Path -f (Invoke-Expression $requirement.Path_f)) -Parent) -Parent)
+            New-Item -ItemType Directory -Force -Path $Parent | Write-Debug
+
             Add-Type -Assembly 'System.IO.Compression.FileSystem'
-            [IO.Compression.ZipFile]::ExtractToDirectory((Resolve-Path 'requirement.zip'), (Get-Location))
+            [IO.Compression.ZipFile]::ExtractToDirectory((Resolve-Path 'requirement.zip'), (Resolve-Path $Parent))
+            
             Remove-Item 'requirement.zip'
         }
 
