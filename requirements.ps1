2ddeff4145f7f8c077025df82dd77a0af50c35d5
Raymond Piller <VertigoRay@users.noreply.github.com>
2015-08-13 16:44:53 -0500

    Improved Debug Messages

--- requirements.ps1
+++ requirements.ps1
@@ -1,7 +1,6 @@
 foreach ($requirement in (ConvertFrom-Json (Get-Content .\REQUIREMENTS.json | Out-String))) {
     Write-Debug ($requirement | Out-String)
     if (-not (Get-Command $requirement.Command -ErrorAction Ignore)) {
-        Write-Debug ($requirement | Out-String)
         if (-not (Test-Path ($requirement.Path -f (Invoke-Expression $requirement.Path_f)) -ErrorAction Ignore)) {
             Invoke-WebRequest ($requirement.URL -f (Invoke-Expression $requirement.URL_f)) -OutFile 'requirement.zip' -UseBasicParsing
 
@@ -10,10 +9,11 @@ foreach ($requirement in (ConvertFrom-Json (Get-Content .\REQUIREMENTS.json | Ou
 
             Add-Type -Assembly 'System.IO.Compression.FileSystem'
             [IO.Compression.ZipFile]::ExtractToDirectory((Resolve-Path 'requirement.zip'), (Resolve-Path $Parent))
-            
+
             Remove-Item 'requirement.zip'
         }
 
+        Write-Debug "Importing: $($requirement.Path -f (Invoke-Expression $requirement.Path_f))"
         . ($requirement.Path -f (Invoke-Expression $requirement.Path_f))
     }
 }
